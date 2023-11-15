c0bba4b718f6652d5c690ecfb215f01cc30222be
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-11-15 15:25:27 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,54 @@
+module github.com/easterthebunny/link-token-test
+
+go 1.21.3
+
+require github.com/ethereum/go-ethereum v1.13.5
+
+require github.com/yusufpapurcu/wmi v1.2.3 // indirect
+
+require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/bits-and-blooms/bitset v1.7.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-kzg-4844 v0.7.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.3.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/ethereum/c-kzg-4844 v0.4.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/smartcontractkit/chainlink/v2 v2.7.0
+	github.com/supranational/blst v0.3.11 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
+
+replace (
+	// Fix go mod tidy issue for ambiguous imports from go-ethereum
+	// See https://github.com/ugorji/go/issues/279
+	github.com/btcsuite/btcd => github.com/btcsuite/btcd v0.22.1
+	github.com/cosmos/ibc-go/v7 => github.com/cosmos/ibc-go/v7 v7.3.0
+
+	// replicating the replace directive on cosmos SDK
+	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+
+	// until merged upstream: https://github.com/hashicorp/go-plugin/pull/257
+	github.com/hashicorp/go-plugin => github.com/smartcontractkit/go-plugin v0.0.0-20230605132010-0f4d515d1472
+
+	// until merged upstream: https://github.com/mwitkow/grpc-proxy/pull/69
+	github.com/mwitkow/grpc-proxy => github.com/smartcontractkit/grpc-proxy v0.0.0-20230731113816-f1be6620749f
+)
---
+++ main.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/big"
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/shared/generated/link_token"
+)
+
+type Config struct {
+	HTTPURL            string `json:"http_url"`
+	ChainID            int64  `json:"chain_id"`
+	OwnerAddress       string `json:"owner_address"`
+	OwnerPrivateKey    string `json:"owner_private_key"`
+	ReceiverAddress    string `json:"receiver_address"`
+	ReceiverPrivateKey string `json:"receiver_private_key"`
+	DeployContract     bool   `json:"deploy_contract"`
+	ContractAddress    string `json:"contract_address"`
+	Mint               bool   `json:"mint"`
+}
+
+func main() {
+	conf := mustReadConfig("config.json")
+
+	rpcClient, err := rpc.Dial(conf.HTTPURL)
+	if err != nil {
+		log.Fatalf("dial rpc: %s", err.Error())
+	}
+
+	ownerClient := &client{
+		rpc:     ethclient.NewClient(rpcClient),
+		key:     privateKey(conf.OwnerPrivateKey),
+		chainID: big.NewInt(conf.ChainID),
+	}
+
+	ctx := context.Background()
+
+	var contract *link_token.LinkToken
+
+	if conf.DeployContract {
+		var err error
+
+		if contract, err = deploy(ctx, ownerClient); err != nil {
+			panic(err)
+		}
+	} else {
+		var err error
+
+		if contract, err = link_token.NewLinkToken(common.HexToAddress(conf.ContractAddress), ownerClient.rpc); err != nil {
+			log.Fatalf("connect to token contract failure: %s", err.Error())
+		}
+	}
+
+	if conf.Mint {
+		if err := ensureMintingRoleFor(ctx, contract, ownerClient, conf.OwnerAddress); err != nil {
+			panic(err)
+		}
+
+		if err := mintAmountTo(ctx, contract, ownerClient, 100_000_000_000_000_000, conf.OwnerAddress); err != nil {
+			panic(err)
+		}
+	}
+
+	receiverClient := &client{
+		rpc:     ethclient.NewClient(rpcClient),
+		key:     privateKey(conf.ReceiverPrivateKey),
+		chainID: big.NewInt(conf.ChainID),
+	}
+
+	// print balance of sender and send
+	mustPrintBalance(ctx, contract, conf.OwnerAddress)
+	mustApprove(ctx, contract, ownerClient, 10, conf.OwnerAddress, conf.ReceiverAddress)
+
+	// print balance of receiver and run
+	mustPrintBalance(ctx, contract, conf.ReceiverAddress)
+	mustReceive(ctx, contract, receiverClient, 6, conf.OwnerAddress, conf.ReceiverAddress)
+
+	// print both balances after
+	mustPrintBalance(ctx, contract, conf.OwnerAddress)
+	mustPrintBalance(ctx, contract, conf.ReceiverAddress)
+}
+
+type client struct {
+	rpc     *ethclient.Client
+	key     *ecdsa.PrivateKey
+	chainID *big.Int
+}
+
+func buildTxOpts(ctx context.Context, client *client) (*bind.TransactOpts, error) {
+	var address common.Address
+
+	nonce, err := client.rpc.PendingNonceAt(ctx, address)
+	if err != nil {
+		return nil, err
+	}
+
+	gasPrice, err := client.rpc.SuggestGasPrice(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(client.key, client.chainID)
+	if err != nil {
+		return nil, err
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0) // in wei
+	auth.GasLimit = 0
+	auth.GasPrice = gasPrice
+
+	return auth, nil
+}
+
+func deploy(ctx context.Context, client *client) (*link_token.LinkToken, error) {
+	opts, err := buildTxOpts(ctx, client)
+	if err != nil {
+		return nil, err
+	}
+
+	_, trx, contract, err := link_token.DeployLinkToken(opts, client.rpc)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := bind.WaitDeployed(ctx, client.rpc, trx); err != nil {
+		return nil, err
+	}
+
+	return contract, nil
+}
+
+func ensureMintingRoleFor(ctx context.Context, contract *link_token.LinkToken, cl *client, addr string) error {
+	minters, err := contract.GetMinters(&bind.CallOpts{Context: ctx})
+	if err != nil {
+		return err
+	}
+
+	var ownerIsMinter bool
+
+	for _, minter := range minters {
+		if minter.Hex() == addr {
+			ownerIsMinter = true
+
+			break
+		}
+	}
+
+	if !ownerIsMinter {
+		opts, err := buildTxOpts(ctx, cl)
+		if err != nil {
+			return err
+		}
+
+		trx, err := contract.GrantMintRole(opts, common.HexToAddress(addr))
+		if err != nil {
+			return err
+		}
+
+		receipt, err := bind.WaitMined(ctx, cl.rpc, trx)
+		if err != nil {
+			return err
+		}
+
+		if receipt.Status == types.ReceiptStatusFailed {
+			return fmt.Errorf("failed status receipt: %d", receipt.Status)
+		}
+	}
+
+	return nil
+}
+
+func mintAmountTo(ctx context.Context, contract *link_token.LinkToken, cl *client, amt int64, to string) error {
+	opts, err := buildTxOpts(ctx, cl)
+	if err != nil {
+		return err
+	}
+
+	trx, err := contract.Mint(opts, common.HexToAddress(to), big.NewInt(amt))
+	if err != nil {
+		return err
+	}
+
+	receipt, err := bind.WaitMined(ctx, cl.rpc, trx)
+	if err != nil {
+		return err
+	}
+
+	if receipt.Status == types.ReceiptStatusFailed {
+		return fmt.Errorf("failed status receipt: %d", receipt.Status)
+	}
+
+	return nil
+}
+
+func mustPrintBalance(ctx context.Context, contract *link_token.LinkToken, addr string) {
+	balance, err := contract.BalanceOf(&bind.CallOpts{Context: ctx}, common.HexToAddress(addr))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s balance: %s juels\n", addr, balance.String())
+}
+
+func mustApprove(ctx context.Context, contract *link_token.LinkToken, cl *client, amt int64, from, to string) {
+	opts, err := buildTxOpts(ctx, cl)
+	if err != nil {
+		panic(err)
+	}
+
+	trx, err := contract.Approve(opts, common.HexToAddress(to), big.NewInt(amt))
+	if err != nil {
+		panic(err)
+	}
+
+	receipt, err := bind.WaitMined(ctx, cl.rpc, trx)
+	if err != nil {
+		panic(err)
+	}
+
+	if receipt.Status == types.ReceiptStatusFailed {
+		panic(fmt.Errorf("failed status receipt: %d", receipt.Status))
+	}
+
+	allowed, err := contract.Allowance(&bind.CallOpts{Context: ctx}, common.HexToAddress(from), common.HexToAddress(to))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s juels allowed from %s to %s\n", allowed.String(), from, to)
+}
+
+func mustReceive(ctx context.Context, contract *link_token.LinkToken, cl *client, amt int64, from, to string) {
+	allowed, err := contract.Allowance(&bind.CallOpts{Context: ctx}, common.HexToAddress(from), common.HexToAddress(to))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s juels allowed from %s to %s\n", allowed.String(), from, to)
+
+	opts, err := buildTxOpts(ctx, cl)
+	if err != nil {
+		panic(err)
+	}
+
+	trx, err := contract.TransferFrom(opts, common.HexToAddress(from), common.HexToAddress(to), big.NewInt(amt))
+	if err != nil {
+		panic(err)
+	}
+
+	receipt, err := bind.WaitMined(ctx, cl.rpc, trx)
+	if err != nil {
+		panic(err)
+	}
+
+	if receipt.Status == types.ReceiptStatusFailed {
+		panic(fmt.Errorf("failed status receipt: %d", receipt.Status))
+	}
+
+	allowed, err = contract.Allowance(&bind.CallOpts{Context: ctx}, common.HexToAddress(from), common.HexToAddress(to))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s juels allowed from %s to %s\n", allowed.String(), from, to)
+}
+
+func mustReadConfig(path string) Config {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	confBytes, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var conf Config
+	if err := json.Unmarshal(confBytes, &conf); err != nil {
+		panic(err)
+	}
+
+	return conf
+}
+
+func privateKey(key string) *ecdsa.PrivateKey {
+	pkBase := new(big.Int).SetBytes(common.FromHex(strings.TrimSpace(key)))
+	pkX, pkY := crypto.S256().ScalarBaseMult(pkBase.Bytes())
+
+	return &ecdsa.PrivateKey{
+		PublicKey: ecdsa.PublicKey{
+			Curve: crypto.S256(),
+			X:     pkX,
+			Y:     pkY,
+		},
+		D: pkBase,
+	}
+}
